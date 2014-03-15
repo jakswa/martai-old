fdb5df6a966ac1c73eb99e9aa6a3009c732e8b76
Jake Swanson <jakswa@gmail.com>
2014-03-15 01:00:50 -0400

    so tired of dealing with marta's uppercase keys...
    maybe we'll provide a raw proxy at some point, but for now, suck it

--- app/scripts/controllers/station.js
+++ app/scripts/controllers/station.js
@@ -2,5 +2,6 @@
 
 angular.module('martaioApp')
   .controller('StationCtrl', function ($scope, $http, $routeParams, Marta) {
+    $scope.station = $routeParams.station;
   	$scope.Marta = Marta;
   });
