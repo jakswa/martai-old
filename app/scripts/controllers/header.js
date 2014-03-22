843799a7906160a960d3e903597c74926ceaed6a
Jake Swanson <jakswa@gmail.com>
2014-03-22 02:33:13 -0400

    the back button now properly does history.back()

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('martaioApp')
-  .controller('HeaderCtrl', function ($scope, $location, Marta, User) {
+  .controller('HeaderCtrl', function ($scope, $location, $window, Marta, User) {
     $scope.location = $location;
     $scope.Marta = Marta;
     $scope.user = User.data();
     $scope.openMenu = false;
     $scope.showBackBtn = false;
+    $scope.$window = $window;
 
     $scope.$watch('location.path()', function (val) {
       $scope.showBackBtn = (val !== '/');
